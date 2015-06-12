9927734666b906fe85a0482a07bfca4fc410f0bd
CaioViktor <caioviktor@hotmail.com>
2015-06-12 18:25:04 -0300

    Dado

--- src/Dado.java
+++ src/Dado.java
@@ -1,5 +1,50 @@
 public class Dado{
-	public void removeFilaEspera(Transacao transacao){
+	private String identificador;
+	private FilaTransacao filaEspera;
+	private Transacao bloqueioLeitura;
+	private Transacao bloqueioEscrita;
+	public Dado(String identificador){
+		this.identificador = identificador;
+		filaEspera = new FilaTransacao();
+		bloqueioEscrita = null;
+		bloqueioLeitura = null;
+	}
+	public String getIdentificador(){
+		return identificador;
+	}
+	public boolean isBloqueadaLeitura(){
+		if(bloqueioLeitura == null)
+			return false;
+		return true;
+	}
+	public boolean isBloqueadaEscrita(){
+		if(bloqueioEscrita == null)
+			return false;
+		return true;
+	}
+	public Transacao getBloqueioLeitura(){
+		return bloqueioLeitura;
+	}
+	public Transacao getBloqueioEscrita(){
+		return bloqueioEscrita;
+	}
+	public void setBloqueioLeitura(Transacao transacao){
+		bloqueioLeitura = transacao;
+	}
+	public void setBloqueioEscrita(Transacao transacao){
+		bloqueioEscrita = transacao;
+	}
 
+	public void addFilaEspera(Transacao transacao){
+		// filaEspera.append(transacao);
+	}
+	public void removeFilaEspera(Transacao transacao){
+		// filaEspera.removeTransacao(transacao);
+	}
+	public void removeFilaEspera(){
+		// filaEspera.removeInicio();
+	}
+	public void notificarFilaEspera(){
+		// filaEspera.getInicio().start(filaEspera.getInicio().getTimestamp());
 	}
 }
